b8b26f78410fa9223557d4f5e25d98c658e5e0bf
akashsolanki1101 <akashsolanki2509@gmail.com>
2020-04-15 00:11:54 +0530

    Landing page is made responsive for mobiles

--- src/components/Home/LandingDisplay/LandingDisplay.jsx
+++ src/components/Home/LandingDisplay/LandingDisplay.jsx
@@ -4,19 +4,21 @@ import Grid from '@material-ui/core/Grid'
 import Container from '@material-ui/core/Container'
 import classes from './LandingDisplay.module.css'
 
+
 export default function LandingDisplay() {
     return (
         <div className={classes.main_display}>
             <Container style={{height : "100%"}}>
                 <Grid container style={{height : "100%"}}>
-                    <Grid item xs = {6}>
+                    
+                <Grid item xs = {12} sm={6}>
                         <div className={classes.left_part}>
-                            <span className={classes.title}>Curated Cart</span>
-                            <span className={classes.subTitle}>REASON OF YOUR HAPPINESS</span>
+                        <p className={classes.title}>Curated Cart</p>
+                        <p className={classes.subTitle}>REASON OF YOUR HAPPINESS</p>
                         </div>
                     </Grid>
 
-                    <Grid item xs={6}>
+                    <Grid item xs={12} sm={6}>
                         <div className={classes.right_part}>
                             
                         </div>
